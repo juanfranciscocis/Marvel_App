562c36e4b144a123bd275a15406cf528423f12f2
Juan Francisco Cisneros <juanfrancistm2011@icloud.com>
2023-09-25 20:02:48 -0500

    CUENTA LISTO

--- appServer/controllers/cuenta.js
+++ appServer/controllers/cuenta.js
@@ -1,8 +1,18 @@
 //controllers
 
-// homepage - GET
+// cuenta - GET
+cuenta_page = {
+    imagen:"../images/cuenta/usuario.png",
+    nombre:"Juan Francisco Cisneros",
+    nombre_usuario:"JuanCisneros",
+    correo:"juanfrancistm2011@icloud.com",
+}
+
+
+
+
 const cuenta = (req, res, next) =>{
-    res.render('cuenta', { title: 'CUENTA' });
+    res.render('cuenta', { title: 'CUENTA', cuenta_page });
 }
 
 module.exports = {
